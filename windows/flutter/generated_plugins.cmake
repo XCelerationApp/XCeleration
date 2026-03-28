3e391486a1e09793e7768c89f8062c312a3df1e7
Teo <teodonnelley@gmail.com>
2026-03-28 15:26:39 -0700

    Feat - Update dependencies and register new plugins across platforms
    - Upgraded AppAuth to version 2.0.0 and GoogleSignIn to version 9.1.0 in Podfile.lock.
    - Added DesktopWebviewWindowPlugin registration in generated_plugin_registrant.cc for Linux, macOS, and Windows.
    - Updated generated_plugins.cmake to include DesktopWebviewWindow for Linux and Windows.
    - Included new imports for desktop_webview_window and flutter_inappwebview_macos in macOS plugin registration.
    - Minor updates to other dependencies and their checksums.

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,6 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   app_links
   audioplayers_windows
   connectivity_plus
+  desktop_webview_window
   flutter_inappwebview_windows
   flutter_sound
   geolocator_windows
